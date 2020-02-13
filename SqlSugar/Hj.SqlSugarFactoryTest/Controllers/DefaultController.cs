1c5d36601ad94a3d41bb305fb5922b2efce8ab9e
黄杰 <Admin@DESKTOP-UJPI3F8>
2020-02-13 18:10:03 +0800

    sqlSugar  AOP测试

--- SqlSugar/Hj.SqlSugarFactoryTest/Controllers/DefaultController.cs
+++ SqlSugar/Hj.SqlSugarFactoryTest/Controllers/DefaultController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Hj.SqlSugarFactory;
+using Hj.SqlSugarFactoryTest.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SqlSugar;
@@ -16,12 +17,24 @@ namespace Hj.SqlSugarFactoryTest.Controllers
         public SqlSugarClient _db;
         public DefaultController(ISqlSugarFactory sqlSugarFactory)
         {
-            _db = sqlSugarFactory.CreateClient("LogsConnection");//Database=DGCN.HXCloud.ID4
+            _db = sqlSugarFactory.CreateClient("Localhost");//Database=DGCN.HXCloud.ID4
+            _db.Aop.OnDiffLogEvent = it =>
+            {
+                var editBeforeData = it.BeforeData;
+                var editAfterData = it.AfterData;
+                var sql = it.Sql;
+                var parameter = it.Parameters;
+                var data = it.BusinessData;
+                var time = it.Time;
+                var diffType = it.DiffType;//枚举值 insert 、update 和 delete 用来作业务区分
+
+                //你可以在这里面写日志方法
+            };
         }
         [HttpGet]
         public string Get()
         {
-            //var departQueryable = _db.Queryable<Department>().Where(x => x.IsDelete == false).ToList();
+            var t12 = _db.Insertable<Test>(new { Id=2, Name = "a" }).EnableDiffLogEvent(new { title = "add test" }).ExecuteCommand();
             return "string";
         }
     }

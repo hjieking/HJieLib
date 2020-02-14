939762269b34e993ce76ba77a3f48c637c61bdcf
黄杰 <Admin@DESKTOP-UJPI3F8>
2020-02-14 15:57:24 +0800

    数据日志初步优化

--- SqlSugar/Hj.SqlSugarFactory/Lib/MySqlSugarClient.cs
+++ SqlSugar/Hj.SqlSugarFactory/Lib/MySqlSugarClient.cs
@@ -1,4 +1,5 @@
-﻿using SqlSugar;
+﻿using Newtonsoft.Json;
+using SqlSugar;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
@@ -10,6 +11,48 @@ namespace Hj.SqlSugarFactory.Lib
     {
         public MySqlSugarClient(ConnectionConfig config) : base(config)
         {
+        }
+        /// <summary>
+        /// 进行数据日志测试
+        /// </summary>
+        /// <param name="sqlSugarClient">记录数据日志客户端</param>
+        /// <param name="mySqlSugarClient">控制器使用的客户端</param>
+        public void AopDataLog(SqlSugarClient sqlSugarClient, MySqlSugarClient mySqlSugarClient)
+        {
+            mySqlSugarClient.Aop.OnDiffLogEvent = it =>
+            {
+                var editBeforeData = it.BeforeData;
+                var editAfterData = it.AfterData;
+                var sql = it.Sql;
+                var parameter = it.Parameters;
+                var data = it.BusinessData;//表名
+                var time = it.Time;
+                var diffType = it.DiffType;//枚举值 insert 、update 和 delete 用来作业务区分
+
+                //你可以在这里面写日志方法
+                var oldValueJson = "";
+                if (editBeforeData != null && editBeforeData.Count != 0)
+                {
+                    oldValueJson = JsonConvert.SerializeObject(editBeforeData[0].Columns);
+                }
+
+                var newValueJson = "";
+                if (editAfterData != null && editAfterData.Count != 0)
+                {
+                    newValueJson = JsonConvert.SerializeObject(editAfterData[0].Columns);
+                }
+                var model = new DataLog()
+                {
+                    ID = Guid.NewGuid(),
+                    KeyValue = data.ToString(),
+                    OldValue = oldValueJson != "" ? oldValueJson : null,
+                    NewValue = newValueJson != "" ? newValueJson : null,
+                    OperateType = diffType.ToString(),
+                    UserName = "test",
+                    OperateTime = DateTime.Now.ToString()
+                };
+                sqlSugarClient.Insertable(model).ExecuteCommand();
+            };
         }
         #region Insertable
         public  IInsertable<T> Insertable<T>(Dictionary<string, object> columnDictionary) where T : class, new()
--- SqlSugar/Hj.SqlSugarFactoryTest/Controllers/DefaultController.cs
+++ SqlSugar/Hj.SqlSugarFactoryTest/Controllers/DefaultController.cs
@@ -22,40 +22,42 @@ namespace Hj.SqlSugarFactoryTest.Controllers
         {
             _db = (MySqlSugarClient)sqlSugarFactory.CreateClient("LogsConnection");//Database=DGCN.HXCloud.ID4  Localhost LogsConnection
              db = sqlSugarFactory.CreateClient("LogsConnection");
-            _db.Aop.OnDiffLogEvent = it =>
-            {
-                var editBeforeData = it.BeforeData;
-                var editAfterData = it.AfterData;
-                var sql = it.Sql;
-                var parameter = it.Parameters;
-                var data = it.BusinessData;//表名
-                var time = it.Time;
-                var diffType = it.DiffType;//枚举值 insert 、update 和 delete 用来作业务区分
 
-                //你可以在这里面写日志方法
-                var oldValueJson = "";
-                if (editBeforeData != null&&editBeforeData.Count!=0)
-                {
-                    oldValueJson = JsonConvert.SerializeObject(editBeforeData[0].Columns);
-                }
+            _db.AopDataLog(db, _db);
+            //_db.Aop.OnDiffLogEvent = it =>
+            //{
+            //    var editBeforeData = it.BeforeData;
+            //    var editAfterData = it.AfterData;
+            //    var sql = it.Sql;
+            //    var parameter = it.Parameters;
+            //    var data = it.BusinessData;//表名
+            //    var time = it.Time;
+            //    var diffType = it.DiffType;//枚举值 insert 、update 和 delete 用来作业务区分
 
-                var newValueJson = "" ;
-                if (editAfterData != null && editAfterData.Count != 0)
-                {
-                   newValueJson= JsonConvert.SerializeObject(editAfterData[0].Columns);
-                }
-                var model = new DataLog()
-                {
-                    ID = Guid.NewGuid(),
-                    KeyValue = data.ToString(),
-                    OldValue = oldValueJson != "" ? oldValueJson : null,
-                    NewValue = newValueJson != "" ? newValueJson : null,
-                    OperateType = diffType.ToString(),
-                    UserName = "test",
-                    OperateTime = DateTime.Now.ToString()
-                };
-                db.Insertable(model).ExecuteCommand();
-            };
+            //    //你可以在这里面写日志方法
+            //    var oldValueJson = "";
+            //    if (editBeforeData != null&&editBeforeData.Count!=0)
+            //    {
+            //        oldValueJson = JsonConvert.SerializeObject(editBeforeData[0].Columns);
+            //    }
+
+            //    var newValueJson = "" ;
+            //    if (editAfterData != null && editAfterData.Count != 0)
+            //    {
+            //       newValueJson= JsonConvert.SerializeObject(editAfterData[0].Columns);
+            //    }
+            //    var model = new DataLog()
+            //    {
+            //        ID = Guid.NewGuid(),
+            //        KeyValue = data.ToString(),
+            //        OldValue = oldValueJson != "" ? oldValueJson : null,
+            //        NewValue = newValueJson != "" ? newValueJson : null,
+            //        OperateType = diffType.ToString(),
+            //        UserName = "test",
+            //        OperateTime = DateTime.Now.ToString()
+            //    };
+            //    db.Insertable(model).ExecuteCommand();
+            //};
         }
         [HttpGet("Get")]
         public string Get()
